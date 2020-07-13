41d8d1464f051e41c719ffa97455d0ea74a1ba28
justin_rhuebottom <no-reply>
2020-07-13 16:23:43 -0400

    optimize find duplicates

--- tuneup.py
+++ tuneup.py
@@ -5,20 +5,28 @@
 Use the timeit and cProfile libraries to find bad code.
 """
 
-__author__ = "???"
+__author__ = "Justin Rhuebottom"
 
 import cProfile
 import pstats
 import functools
+import timeit
 
 
 def profile(func):
     """A cProfile decorator function that can be used to
     measure performance.
     """
-    # Be sure to review the lesson material on decorators.
-    # You need to understand how they are constructed and used.
-    raise NotImplementedError("Complete this decorator function")
+    def wrapper(*args, **kw):
+        pr = cProfile.Profile()
+        pr.enable()
+        x = pr.runcall(func, *args, **kw)
+        pr.disable()
+        # pr.print_stats()
+        Stats = pstats.Stats(pr).sort_stats("cumulative")
+        Stats.print_stats()
+        return x
+    return wrapper
 
 
 def read_movies(src):
@@ -36,21 +44,29 @@ def is_duplicate(title, movies):
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list."""
     movies = read_movies(src)
     duplicates = []
+    # while movies:
+    #     movie = movies.pop()
+    #     if is_duplicate(movie, movies):
+    #         duplicates.append(movie)
     while movies:
         movie = movies.pop()
-        if is_duplicate(movie, movies):
+        if movie in movies:
             duplicates.append(movie)
     return duplicates
 
 
 def timeit_helper():
     """Part A: Obtain some profiling measurements using timeit."""
-    # YOUR CODE GOES HERE
-    pass
+    t = timeit.Timer(setup="from __main__ import main", stmt="main()")
+    result = t.repeat(repeat=3, number=10)
+    result.sort()
+    bestTime = result[0] / 10
+    print(f"Best time across 3 repeats of 10 runs per repeat: {bestTime} sec")
 
 
 def main():
@@ -61,4 +77,5 @@ def main():
 
 
 if __name__ == '__main__':
+    timeit_helper()
     main()
